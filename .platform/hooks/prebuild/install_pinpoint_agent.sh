56ed9b02aa8b98e8f34bd039aea5776a6444c0e8
kth <dms304@outlook.kr>
2025-08-26 02:24:35 +0900

    핀포인트 폴더 위치 변경 - 오타 수정

--- .platform/hooks/prebuild/install_pinpoint_agent.sh
+++ .platform/hooks/prebuild/install_pinpoint_agent.sh
@@ -7,7 +7,7 @@ if [ ! -d PINPOINT_DIRECTORY ]; then
     wget https://github.com/pinpoint-apm/pinpoint/releases/download/v2.5.1/pinpoint-agent-2.5.1.tar.gz
 
     # 압축 풀기 (beanstalk의 application.jar가 있는 곳)
-    tar -zxvf pinpoint-agent-2.5.1.tar.gz -C /var/app/current
+    sudo tar -zxvf pinpoint-agent-2.5.1.tar.gz -C /var/app/current
 
     # 설정파일 경로
     CONFIG_PATH="$PINPOINT_DIRECTORY/profiles/release/pinpoint.config"
@@ -16,6 +16,6 @@ if [ ! -d PINPOINT_DIRECTORY ]; then
 
     # 모니터링 서버 IP 변경
     # 여기서 {ip} 부분을 실제 Collector 서버 IP로 바꿔야 함
-    sed -i "s|profiler.transport.grpc.collector.ip=.*|profiler.transport.grpc.collector.ip=$MONITORING_SERVER_IP|" "$CONFIG_PATH"
+    sudo sed -i "s|profiler.transport.grpc.collector.ip=.*|profiler.transport.grpc.collector.ip=$MONITORING_SERVER_IP|" "$CONFIG_PATH"
 
 fi
