bf29c50592a6c92d6a9a450c88ab2f7d89168a75
kth <dms304@outlook.kr>
2025-08-25 22:02:09 +0900

    pinpoint 실행 안되는 이슈 고치기

---
+++ .platform/hooks/prebuild/install_pinpoint_agent.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 디렉토리 체크: []와 경로 사이에 공백 필요
+if [ ! -d /home/ec2-user/pinpoint-agent-2.5.1 ]; then
+
+    # Pinpoint Agent 다운로드
+    wget https://github.com/pinpoint-apm/pinpoint/releases/download/v2.5.1/pinpoint-agent-2.5.1.tar.gz
+
+    # 압축 풀기
+    tar -zxvf pinpoint-agent-2.5.1.tar.gz -C /home/ec2-user/
+
+    # 설정파일 경로
+    CONFIG_PATH="/home/ec2-user/pinpoint-agent-2.5.1/profiles/release/pinpoint.config"
+
+    MONITORING_SERVER_IP="http://43.202.5.63/"
+
+    # 모니터링 서버 IP 변경
+    # 여기서 {ip} 부분을 실제 Collector 서버 IP로 바꿔야 함
+    sed -i "s/profiler.transport.grpc.collector.ip=.*/profiler.transport.grpc.collector.ip=$MONITORING_SERVER_IP/" "$CONFIG_PATH"
+
+fi
