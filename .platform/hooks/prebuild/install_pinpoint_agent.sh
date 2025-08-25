fc7ac37025490d56039d100c110169a596f26d76
kth <dms304@outlook.kr>
2025-08-26 02:06:07 +0900

    핀포인트 폴더 위치 변경 - 오타 수정

--- .platform/hooks/prebuild/install_pinpoint_agent.sh
+++ .platform/hooks/prebuild/install_pinpoint_agent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+PINPOINT_DIRECTORY="/var/app/current/pinpoint-agent-2.5.1"
 # 디렉토리 체크: []와 경로 사이에 공백 필요
-if [ ! -d /var/app/current/pinpoint-agent-2.5.1 ]; then
+if [ ! -d PINPOINT_DIRECTORY ]; then
 
     # Pinpoint Agent 다운로드
     wget https://github.com/pinpoint-apm/pinpoint/releases/download/v2.5.1/pinpoint-agent-2.5.1.tar.gz
@@ -9,9 +9,8 @@ if [ ! -d /var/app/current/pinpoint-agent-2.5.1 ]; then
     # 압축 풀기
     tar -zxvf pinpoint-agent-2.5.1.tar.gz -C /var/app/current
 
-
     # 설정파일 경로
-    CONFIG_PATH="/home/ec2-user/pinpoint-agent-2.5.1/profiles/release/pinpoint.config"
+    CONFIG_PATH="$PINPOINT_DIRECTORY/profiles/release/pinpoint.config"
 
     MONITORING_SERVER_IP="43.202.5.63"
 
