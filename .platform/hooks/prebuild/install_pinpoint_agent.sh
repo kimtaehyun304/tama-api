122381563a48dd3ba0bbd4c818e9899c21b516b8
kth <dms304@outlook.kr>
2025-08-26 02:51:50 +0900

    핀포인트 폴더 위치 변경 - 오타 수정

--- .platform/hooks/prebuild/install_pinpoint_agent.sh
+++ .platform/hooks/prebuild/install_pinpoint_agent.sh
@@ -2,19 +2,23 @@
 PINPOINT_DIRECTORY="/home/ec2-user/pinpoint-agent-2.5.1"
 LOGFILE=/var/log/pinpoint-prestart.log
 # 디렉토리 체크: []와 경로 사이에 공백 필요
-if [ ! -d PINPOINT_DIRECTORY ]; then
+if [ ! -d "$PINPOINT_DIRECTORY" ]; then
+
+    cd /home/ec2-user
 
     # Pinpoint Agent 다운로드
     wget https://github.com/pinpoint-apm/pinpoint/releases/download/v2.5.1/pinpoint-agent-2.5.1.tar.gz
 
     # 압축 풀기 (beanstalk의 application.jar가 있는 곳)
-    tar -zxvf /var/app/current/pinpoint-agent-2.5.1.tar.gz -C /home/ec2-user >> $LOGFILE 2>&1 || {
+    tar -zxvf pinpoint-agent-2.5.1.tar.gz >> $LOGFILE 2>&1 || {
         echo "Failed to extract Pinpoint Agent!" >> $LOGFILE 2>&1
         exit 1
     }
 
+    sudo rm -rf pinpoint-agent-2.5.1.tar.gz;
+
     # 설정파일 경로
-    CONFIG_PATH="$PINPOINT_DIRECTORY/profiles/release/pinpoint.config"
+    CONFIG_PATH="pinpoint-agent-2.5.1/profiles/release/pinpoint.config"
 
     MONITORING_SERVER_IP="43.202.5.63"
 
