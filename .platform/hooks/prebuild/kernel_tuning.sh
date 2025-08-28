5d4274fd396db2b459246847b1a6841a992adf83
kth <dms304@outlook.kr>
2025-08-29 04:11:29 +0900

    worker_connections  1024 -> 4096;

--- .platform/hooks/prebuild/kernel_tuning.sh
+++ .platform/hooks/prebuild/kernel_tuning.sh
@@ -5,7 +5,7 @@ set -e
 echo "10240 65535" > /proc/sys/net/ipv4/ip_local_port_range
 
 # tcp 처리량 늘리기
-sudo sysctl -w net.core.somaxconn=8192
-sudo sysctl net.ipv4.tcp_max_syn_backlog=8192
+sudo sysctl -w net.core.somaxconn=16384
+sudo sysctl net.ipv4.tcp_max_syn_backlog=16384
 
 sudo sysctl net.ipv4.tcp_tw_reuse=1
