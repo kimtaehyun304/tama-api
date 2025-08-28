fd3a3ec360e7501734f5caf63f4d2a3fda3e6cf1
kth <dms304@outlook.kr>
2025-08-28 23:59:16 +0900

    nginx keepalive 적용

--- .platform/hooks/prebuild/kernel_tuning.sh
+++ .platform/hooks/prebuild/kernel_tuning.sh
@@ -7,3 +7,5 @@ echo "10240 65535" > /proc/sys/net/ipv4/ip_local_port_range
 # tcp 처리량 늘리기
 sudo sysctl -w net.core.somaxconn=8192
 sudo sysctl net.ipv4.tcp_max_syn_backlog=8192
+
+sudo sysctl net.ipv4.tcp_tw_reuse=1
