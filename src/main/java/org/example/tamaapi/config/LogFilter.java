29e31ff88572de9feef6f83ad4a420791d430bb5
kth <dms304@outlook.kr>
2025-08-30 03:15:59 +0900

    톰캣이 요청을 버리는지 체크하기 위해 로그 필터 추가

--- src/main/java/org/example/tamaapi/config/LogFilter.java
+++ src/main/java/org/example/tamaapi/config/LogFilter.java
@@ -7,13 +7,10 @@ import java.io.IOException;
 @Component
 public class LogFilter implements Filter {
 
-    Long cnt = 1L;
-
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
-        System.out.println("cnt = " + cnt);
-        cnt++;
+        System.out.println("welcome");
         chain.doFilter(req, res);
     }
 
