9599a73cf45dcf234099867bcb5c0cd9238ec6ba
kth <dms304@outlook.kr>
2025-09-01 21:25:13 +0900

    nginx worker_connections 65536

--- .platform/hooks/prebuild/kernel_tuning.sh
+++ .platform/hooks/prebuild/kernel_tuning.sh
@@ -7,6 +7,3 @@ echo "10240 65535" > /proc/sys/net/ipv4/ip_local_port_range
 # tcp 처리량 늘리기
 sudo sysctl -w net.core.somaxconn=32768
 sudo sysctl net.ipv4.tcp_max_syn_backlog=32768
-
-# nginx worker_rlimit_nofile 기본값에 맞춤
-sudo ulimit -n 200000
