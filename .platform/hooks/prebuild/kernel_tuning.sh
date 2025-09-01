c03e9c246fe008bbb5b6c7c4de70434e1bbe67e9
kth <dms304@outlook.kr>
2025-09-02 01:35:01 +0900

    somaxconn tcp_max_syn_backlog 8192

--- .platform/hooks/prebuild/kernel_tuning.sh
+++ .platform/hooks/prebuild/kernel_tuning.sh
@@ -5,5 +5,5 @@ set -e
 echo "10240 65535" > /proc/sys/net/ipv4/ip_local_port_range
 
 # tcp 처리량 늘리기
-sudo sysctl -w net.core.somaxconn=8192
-sudo sysctl net.ipv4.tcp_max_syn_backlog=8192
+sudo sysctl -w net.core.somaxconn=4096
+sudo sysctl net.ipv4.tcp_max_syn_backlog=4096
