6d55d1e9629f0c98f297bd110fa9e7c2b0007ae8
kth <dms304@outlook.kr>
2025-09-02 05:19:19 +0900

    프록시 원복 + 튜닝한거 순정으로 원복

--- .platform/hooks/prebuild/kernel_tuning.sh
+++ .platform/hooks/prebuild/kernel_tuning.sh
@@ -4,6 +4,3 @@ set -e
 # 포트 범위 늘리기 (덤으로 ulimit도 증가함)
 echo "10240 65535" > /proc/sys/net/ipv4/ip_local_port_range
 
-# tcp 처리량 늘리기
-sudo sysctl -w net.core.somaxconn=8192
-sudo sysctl net.ipv4.tcp_max_syn_backlog=8192
