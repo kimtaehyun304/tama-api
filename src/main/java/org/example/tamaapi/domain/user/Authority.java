657888cfc82053ee91460735c7f868276c922d0f
kth <dms304@outlook.kr>
2026-02-06 03:27:28 +0900

    Authority ENUM 개선

--- src/main/java/org/example/tamaapi/domain/user/Authority.java
+++ src/main/java/org/example/tamaapi/domain/user/Authority.java
@@ -1,20 +1,14 @@
 package org.example.tamaapi.domain.user;
 
-import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 
 // implements GrantedAuthority getAuthority 어짜피 String 반환이라 제거
-// getAuthority().getAuthority() 이런식으로 되는 문제도 있어서
-@Getter
 public enum Authority {
 
     //@PreAuthorize가 ROLE_000으로 비교하므로
-    MEMBER("ROLE_MEMBER"),
-    ADMIN("ROLE_ADMIN");
+    MEMBER, ADMIN;
 
-    private final String role;
-
-    Authority(String role) {
-        this.role = role;
+    public String getRole() {
+        return "ROLE_" + this.name();
     }
 }
