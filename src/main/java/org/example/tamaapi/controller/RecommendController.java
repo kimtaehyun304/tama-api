c1dbc5ef69194bfa80ed16905e1328e9aa84d2d0
kth <dms304@outlook.kr>
2026-02-26 03:11:37 +0900

    gpt 프롬프트 수정

--- src/main/java/org/example/tamaapi/controller/RecommendController.java
+++ src/main/java/org/example/tamaapi/controller/RecommendController.java
@@ -23,7 +23,6 @@ public class RecommendController {
 
     //gpt를 통한 상품 검색 (문장을 통한 편한 검색 + 유사어 인지 가능한 장점)
     @PostMapping("/api/ai/recommend")
-    @LogExecutionTime
     public RecommendResponse aiRecommend(@RequestBody RecommendRequest recommendRequest) {
         //사용자가 ~~한 상품을 추천해달라고하면 gpt가 이를 분석해서 where절에 쓸 조건을 json으로 반환
         RecommendedSqlCondition sqlCondition = chatClient.prompt(promptLoader.load())
