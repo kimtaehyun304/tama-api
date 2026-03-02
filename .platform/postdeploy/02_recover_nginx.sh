ff61fe08be01847b89f45a694294b8dce0feade4
kth <dms304@outlook.kr>
2026-03-02 22:36:54 +0900

    sh 스크립트에 sudo 권한 추가

--- .platform/postdeploy/02_recover_nginx.sh
+++ .platform/postdeploy/02_recover_nginx.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # systemd가 nginx를 자동으로 시작하려는데, 이미 nignx가 떠있는 경우 때문에
-if ! systemctl is-active --quiet nginx; then
+# .platform 파일 실행은 자동으로 sudo 권한으로 쓰던데, 얘는 자동으로 안 쓰나봄
+if systemctl is-active nginx == failed then
     echo "[WARN] Nginx is not active. try to kill and restart nginx..."
-    pkill -f nginx
-    systemctl start nginx
+    sudo pkill -f nginx
+    sudo systemctl start nginx
     echo "[SUCCESS] Nginx kill and restart successfully."
 fi
