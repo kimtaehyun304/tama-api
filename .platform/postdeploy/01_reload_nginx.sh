7a10b9151489853c10877323d64c0d29ba851d66
kth <dms304@outlook.kr>
2026-03-02 22:52:40 +0900

    postdeploy 테스트

--- .platform/postdeploy/01_reload_nginx.sh
+++ .platform/postdeploy/01_reload_nginx.sh
@@ -2,4 +2,5 @@
 
 #nginx.conf overwrite 이후 적용하기 위해
 #overwrite 전,후 가 같으면 불필요하긴 함
+echo "run 'nginx -s reload'"
 sudo nginx -s reload
--- .platform/postdeploy/02_recover_nginx.sh
+++ .platform/postdeploy/02_recover_nginx.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # systemd가 nginx를 자동으로 시작하려는데, 이미 nignx가 떠있는 경우 때문에
-# .platform 파일 실행은 자동으로 sudo 권한으로 쓰던데, 얘는 자동으로 안 쓰나봄
-if systemctl is-active nginx == failed then
+if ! systemctl is-active nginx; then
     echo "[WARN] Nginx is not active. try to kill and restart nginx..."
     sudo pkill -f nginx
     sudo systemctl start nginx
     echo "[SUCCESS] Nginx kill and restart successfully."
 fi
+echo "systemctl status nginx is active"
