b531a30df345e61881ee6c5b369a662f94744037
kth <dms304@outlook.kr>
2026-07-22 05:01:12 +0900

    nginx 강제 종료 스크립트 수정

--- .platform/hooks/postdeploy/01_reload_nginx.sh
+++ .platform/hooks/postdeploy/01_reload_nginx.sh
@@ -4,4 +4,4 @@
 #overwrite 전,후 가 같으면 불필요하긴 함
 #인증서 자동 갱신후 실행됨
 echo "[INFO] reload nginx"
-sudo nginx -s reload
+nginx -s reload
--- .platform/hooks/prebuild/recover_nginx.sh
+++ .platform/hooks/prebuild/recover_nginx.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# systemd가 nginx를 자동으로 시작하려는데, 이미 nignx가 떠있는 경우 때문에
+# prebuild 단계에서 systemd가 nginx를 자동으로 시작하려는데, 이미 nignx가 떠있는 경우 때문에
+# sudo 붙이면 에러남
 if  systemctl is-active --quiet nginx; then
     echo "[INFO] nginx is active. so skip to restart nginx"
 else
     echo "[WARN] nginx is not active. try to start nginx"
-    sudo pkill -f nginx
-    sudo systemctl start nginx
+    pkill -f nginx
+    systemctl start nginx
     echo "[INFO] nginx restart successfully"
-fi    
+fi
