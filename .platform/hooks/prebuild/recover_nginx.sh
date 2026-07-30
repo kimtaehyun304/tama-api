52140552af0fabd60e7e13ea08d4ac548c8d74d6
kth <dms304@outlook.kr>
2026-07-30 21:13:16 +0900

    pkill -f nginx || true로 실험

--- .platform/hooks/prebuild/recover_nginx.sh
+++ .platform/hooks/prebuild/recover_nginx.sh
@@ -7,7 +7,7 @@ if  systemctl is-active --quiet nginx; then
 else
     echo "[WARN] nginx is not active. try to start nginx"
     #restart로는 먼저 떠있는 nginx 못 죽여서 pkill하는건데, pkill하면 beanstalk 부팅 중지되네;
-    ##pkill -f nginx
-    systemctl restart nginx
+    pkill -f nginx || true
+    systemctl start nginx
     echo "[INFO] nginx restart successfully"
 fi
